b304a624631b98b0e280f35fb024ae03f00ae31d
RecepCelik1 <recep.celik@std.yildiz.edu.tr>
2025-02-22 13:43:49 +0300

    trigger rebuild

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -3,10 +3,12 @@ const { logOutResponse, verifyToken } = require('../utils/authUtils');
 const checkIsAuth = async (req, res, next) => {
     try {
         const sessionToken = req.cookies.sessionToken;
+        console.log(sessionToken);
         const decodedToken = verifyToken(sessionToken);
         req.user = decodedToken;
         next();
     } catch (error) {
+        console.log(error)
         logOutResponse(res);
     }
 };
